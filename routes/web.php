3dcda4bfb9cf1d50edde221e6eb80ce1e3c35ae9
Aayush <aausgh@gmail.com>
2025-02-15 13:07:59 +0545

    managed routes

--- routes/web.php
+++ routes/web.php
@@ -8,20 +8,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+require __DIR__ . '/auth.php';
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [UrlController::class, 'index'])->name('dashboard');
     Route::post('/shorten', [UrlController::class, 'store'])->name('shorten');
 
     Route::get('/qrcode/{shortCode}', [UrlController::class, 'qrCode'])->name('qrcode');
-});
 
-Route::get('/{shortCode}', [UrlController::class, 'redirect'])->name('redirect');
-
-
-Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+Route::get('/{shortCode}', [UrlController::class, 'redirect'])->name('redirect');
